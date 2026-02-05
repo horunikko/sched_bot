910c15f6599ada120f0428f5520fd049f4519d89
horunikko <root@vless-vpn.play2go.cloud>
2026-02-05 14:23:39 +0000

    улучшил проверку клеток

--- fill_sched.py
+++ fill_sched.py
@@ -2,23 +2,25 @@ from openpyxl import load_workbook
 from openpyxl.styles import PatternFill
 import re
 
+
+# цвет, которым будут закрашиваться клетки
 changed_fill = PatternFill(
     start_color="FFFF99",
     end_color="FFFF99",
     fill_type="solid"
 )
 
-
+# нормализуем строку для её проверки
 def normal(value):
     if value is None:
         return ""
-    else:
-        text = str(value)
-        text = text.replace("\n", " ").replace("\r", " ").replace("\t", " ")
-        text = re.sub(r"\s+", " ", text)
-    return str(text).strip().lower()
+    
+    # убираем лишние пакости и в целом приводим к нормальной строке
+    text = re.sub(r"[\s\-—_,/]+", " ", str(value))
+    text = text.replace(" ", "")
+    return text.lower()
 
-
+# функция закраски клетки
 def fill_sched(old_sched, new_sched):
     
     old_wb = load_workbook(old_sched)
@@ -27,6 +29,7 @@ def fill_sched(old_sched, new_sched):
     old_ws = old_wb.active
     new_ws = new_wb.active
     
+    # проверка по совпадению строк
     for row in range(1, new_ws.max_row + 1):
         for col in range(1, new_ws.max_column + 1):
             old_cell = old_ws.cell(row=row, column=col)
